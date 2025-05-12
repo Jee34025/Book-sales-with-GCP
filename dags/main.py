357b86c4e6b22c81a30edb6e78d77f22b4d6cafa
Nirawit Jeein <nirawit.jee@gmail.com>
2025-05-12 16:34:20 +0700

    main env gitignore requirements README(null)

---
+++ dags/main.py
@@ -0,0 +1,115 @@
+from airflow.models import DAG
+from airflow.decorators import dag, task
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.utils.dates import days_ago
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MYSQL_CONNECTION = os.getenv("MYSQL_CONNECTION")   # เชื่อมกับ .env
+CONVERSION_RATE_URL = os.getenv("CONVERSION_RATE_URL")
+
+# ตัวแปรของ output_path ที่จะเซฟ
+mysql_output_path = "/home/airflow/gcs/data/transaction_data_merged.parquet"
+conversion_rate_output_path = "/home/airflow/gcs/data/conversion_rate.parquet"
+final_output_path = "/home/airflow/gcs/data/workshop4_output.parquet"
+
+default_args = {
+    'owner': 'nirawit', #ชื่อเจ้าของโปรเจค
+}
+
+
+@task()
+def get_data_from_mysql(output_path):
+    # รับ output_path มาจาก task ที่เรียกใช้
+
+    # เรียกใช้ MySqlHook เพื่อต่อไปยัง MySQL จาก connection ที่สร้างไว้ใน Airflow
+    mysqlserver = MySqlHook(MYSQL_CONNECTION)
+    
+    # Query จาก database โดยใช้ Hook ที่สร้าง ผลลัพธ์ได้ pandas DataFrame
+    product = mysqlserver.get_pandas_df(sql="SELECT * FROM r2de3.product")
+    customer = mysqlserver.get_pandas_df(sql="SELECT * FROM r2de3.customer")
+    transaction = mysqlserver.get_pandas_df(sql="SELECT * FROM r2de3.transaction")
+
+    # Merge data จาก 2 DataFrame เหมือนใน workshop1
+    merged_transaction = transaction.merge(product, how="left", left_on="ProductNo", right_on="ProductNo").merge(customer, how="left", left_on="CustomerNo", right_on="CustomerNo")
+    
+    # Save ไฟล์ parquet ไปที่ output_path ที่รับเข้ามา
+    # จะไปอยู่ที่ GCS โดยอัตโนมัติ
+    merged_transaction.to_parquet(output_path, index=False)
+    print(f"Output to {output_path}")
+
+
+@task()
+def get_conversion_rate(output_path):
+    # ส่ง request ไป get ข้อมูลจาก CONVERSION_RATE_URL
+    r = requests.get(CONVERSION_RATE_URL)
+    result_conversion_rate = r.json()
+    df = pd.DataFrame(result_conversion_rate)
+    df = df.drop(columns=['id'])
+
+    # แปลง column ให้เป็น date แล้วเซฟไฟล์ parquet
+    df['date'] = pd.to_datetime(df['date'])
+    df.to_parquet(output_path, index=False)
+    print(f"Output to {output_path}")
+
+
+@task()
+def merge_data(transaction_path, conversion_rate_path, output_path):
+    # อ่านจากไฟล์ สังเกตว่าใช้ path จากที่รับ parameter มา
+    transaction = pd.read_parquet(transaction_path)
+    conversion_rate = pd.read_parquet(conversion_rate_path)
+
+    # merge 2 DataFrame
+    final_df = transaction.merge(conversion_rate, how="left", left_on="Date", right_on="date")
+    
+    # แปลงราคา ให้เป็น total_amount และ thb_amount
+    final_df["total_amount"] = final_df["Price"] * final_df["Quantity"]
+    final_df["thb_amount"] = final_df["total_amount"] * final_df["gbp_thb"]
+
+    # drop column ที่ไม่ใช้ และเปลี่ยนชื่อ column
+    final_df = final_df.drop(["date", "gbp_thb"], axis=1)
+
+    final_df.columns = ['transaction_id', 'date', 'product_id', 'price', 'quantity', 'customer_id',
+        'product_name', 'customer_country', 'customer_name', 'total_amount','thb_amount']
+
+    # save ไฟล์ Parquet
+    final_df.to_parquet(output_path, index=False)
+    print(f"Output to {output_path}")
+    
+
+
+@dag(default_args=default_args, schedule_interval="@once", start_date=days_ago(1), tags=["workshop"])
+def workshop5():
+    """
+    ทำการ load ข้อมูลเข้า BigQuery ผ่าน GCSToBigQueryOperator ของ Airflow
+    """
+    
+    t1 = get_data_from_mysql(output_path=mysql_output_path)
+    t2 = get_conversion_rate(output_path=conversion_rate_output_path)
+    t3 = merge_data(
+        transaction_path=mysql_output_path,
+        conversion_rate_path=conversion_rate_output_path,
+        output_path=final_output_path
+    )
+
+    # TODO: สร้าง t4 ที่เป็น GCSToBigQueryOperator เพื่อใช้งานกับ BigQuery แบบ Airflow และใส่ dependencies
+    from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
+    t4 = GCSToBigQueryOperator(
+        task_id='gcs_to_bq_example',
+        bucket='asia-southeast1-workshop5-6597ee11-bucket',
+        source_objects=['data/workshop4_output.parquet'],
+        destination_project_dataset_table='ws5.transaction',
+        source_format='PARQUET',
+        write_disposition='WRITE_TRUNCATE',  # หรือ 'WRITE_APPEND' ก็ได้
+        
+    )
+
+    [t1, t2] >> t3 >> t4
+
+ิbooksalesworkshop()
